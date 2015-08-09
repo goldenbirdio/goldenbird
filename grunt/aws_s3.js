036812067028edf5a6336a482deefd8415f5481e
Brendan O'Rourke <orourkebm@gmail.com>
2015-08-09 16:06:33 -0700

    Setup AWS S3 Key structure.

---
+++ grunt/aws_s3.js
@@ -0,0 +1,3 @@
+/**
+ * Created by orourkebm on 8/9/15.
+ */
--- grunt/config.js
+++ grunt/config.js
@@ -1,18 +1,33 @@
 var path = require('path');
 
 module.exports = function(grunt, options) {
+    function getSecretCreds(environment) {
+        try {
+            var secretCreds = grunt.file.read('./configs/secret/' + environment + 'Config.json', {encoding: 'utf-8'});
+            if (secretCreds) {
+                secretCreds = JSON.parse(secretCreds)
+            }
+            return secretCreds
+        } catch (err) {
+            console.log('Error reading secret credentials: ', err);
+            return false;
+        }
+    }
+
     return {
         dev: {
             options: {
                 variables: {
-                    environment: 'dev'
+                    environment: 'dev',
+                    secret: getSecretCreds('dev')
                 }
             }
         },
         prod: {
             options: {
                 variables: {
-                    environment: 'production'
+                    environment: 'production',
+                    secret: getSecretCreds('production')
                 }
             }
         },
